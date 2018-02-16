cecb1553667ced2988a43918d439bbfa61976984
Ali Gholami <aligholami7596@gmail.com>
2018-02-16 23:12:51 +0330

    Fill the missing data with fillna

--- src/network.py
+++ src/network.py
@@ -28,6 +28,9 @@ print(TRAIN_DATASET.isnull().values.any())
 # Insert a missing data into the cabin C148 and ticket 111369 in its Fare column
 TRAIN_DATASET.loc[(TRAIN_DATASET["Cabin"] == "C148") & (TRAIN_DATASET["Ticket"] == "111369")] = None
 
+# Fill the missing data using filna :D
+TRAIN_DATASET.fillna(0)
+
 # Print the whole dataset
 print(TRAIN_DATASET.loc[(TRAIN_DATASET["Sex"] == "male") & (TRAIN_DATASET["Pclass"] < 2)])
 
