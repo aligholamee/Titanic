efdffc80805098335b8aa6f98cf0494f2ffa463c
Ali Gholami <aligholami7596@gmail.com>
2018-03-05 23:22:58 +0330

    Import models

--- src/network.py
+++ src/network.py
@@ -14,6 +14,14 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.preprocessing import LabelEncoder, StandardScaler
+from sklearn.neural_network import MLPClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.svm import SVC
+from sklearn.gaussian_process import GaussianProcessClassifier
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import RandomForestClassifier, AdaBoostClassifier
+from sklearn.naive_bayes import GaussianNB
+from sklearn.discriminant_analysis import QuadraticDiscriminantAnalysis
 
 DATA_ROOT = './data/'
 SHARP_COUNT = 100
